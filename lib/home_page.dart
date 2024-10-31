00658e2ac75c8d236ac1fbaaaedf49e52c5b3870
Pragat Pandya <pragat.pandya@gmail.com>
2024-10-31 19:57:02 +0530

    Add UI componets for home page.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:voice_assistant/feature_box.dart';
 import 'package:voice_assistant/pallete.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,35 +18,113 @@ class _HomePageState extends State<HomePage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
-                  decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
-                    shape: BoxShape.circle,
-                  ),
-                ),
-              ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/images/virtualAssistant.png',
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            // VIRTUAL ASSISTANT IMAGE
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
                     ),
                   ),
                 ),
+                Container(
+                  height: 123,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/virtualAssistant.png',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  topLeft: Radius.zero,
+                  bottomRight: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
               ),
-            ],
-          ),
-        ],
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Good Morning, what task can I do for you ?',
+                  style: TextStyle(
+                    color: Pallete.mainFontColor,
+                    fontSize: 25,
+                    fontFamily: 'Cera Pro',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(
+                top: 10,
+                left: 22,
+              ),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Here are a few feature',
+                style: TextStyle(
+                  fontFamily: 'Cera Pro',
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // FEATURES' LIST
+            const Column(
+              children: [
+                FeatureBox(
+                  descriptionText:
+                      'A smart way to stay organized and informed with ChatGPT.',
+                  headerText: 'ChatGPT',
+                  color: Pallete.firstSuggestionBoxColor,
+                ),
+                FeatureBox(
+                  descriptionText:
+                      'Get inspired and stay creative with your personal assistant powered by Dall-E.',
+                  headerText: 'Dall-E',
+                  color: Pallete.secondSuggestionBoxColor,
+                ),
+                FeatureBox(
+                  descriptionText:
+                      'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT.',
+                  headerText: 'Smart Voice Assistant',
+                  color: Pallete.secondSuggestionBoxColor,
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: () {},
+        child: const Icon(
+          Icons.mic,
+        ),
       ),
     );
   }
